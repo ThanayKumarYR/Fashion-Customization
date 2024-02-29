a44575edb1fd95eacbad55492be879304309c02c
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-02-29 12:18:54 +0530

    add: deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://thanaykumayr.github.io/Fashion-Customization/",
+  base: "/Fashion-Customization/",
   plugins: [react()],
   build: {
     // generate manifest.json in outDir
